67e8b68cd84bdc04b06b1805aa5789f3888a8d0d
miguelesco <miuelangel1999@gmail.com>
2021-11-30 23:31:49 -0400

    diagram migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+  has_many :entities, dependent: :destroy
+  has_many :groups, dependent: :destroy
 end
