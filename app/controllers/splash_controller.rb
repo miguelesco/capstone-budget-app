2b647e87bc0c3f7e5994aa2fe0af6c0ed1e09802
miguelesco <miuelangel1999@gmail.com>
2021-12-01 18:08:37 -0400

    groups page

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,6 +1,6 @@
 class SplashController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    redirect_to groups_show_url if current_user
+    redirect_to group_index_path if current_user
   end
 end
