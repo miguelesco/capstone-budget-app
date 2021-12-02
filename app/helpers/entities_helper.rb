996fcd921bf68ea3b04979d8cefc844b598497f2
miguelesco <miuelangel1999@gmail.com>
2021-12-01 20:59:49 -0400

    entities

--- app/helpers/entities_helper.rb
+++ app/helpers/entities_helper.rb
@@ -1,2 +1,17 @@
 module EntitiesHelper
+  def entities_total(group)
+    GroupEntity.where("group_id = #{group.id}").joins(:entity).sum('amount')
+  end
+
+  def entity_name(entity)
+    Entity.find(entity.entity_id).name
+  end
+
+  def entity_created_at(entity)
+    Entity.find(entity.entity_id).created_at.strftime('%d %b %Y')
+  end
+
+  def entity_amount(entity)
+    Entity.find(entity.entity_id).amount
+  end
 end
